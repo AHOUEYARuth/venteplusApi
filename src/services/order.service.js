99f9c955835b52d287c3d2af134f8996a3e0574f
adikpetoaristidezeus <adikpetoaristidezeus@gmail.com>
2025-10-31 13:14:51 +0100

    fixed:date end

--- src/services/order.service.js
+++ src/services/order.service.js
@@ -236,8 +236,8 @@ async createOrder(data) {
  
   if (isSale === true) {
     const now = new Date();
-    const startOfMonth = new Date(now.getFullYear(), now.getMonth(), 1);
-    const endOfMonth = new Date(now.getFullYear(), now.getMonth() + 1, 0);
+    const startOfMonth = new Date(now.getFullYear(), now.getMonth(), 1, 0, 0, 0, 0);
+    const endOfMonth = new Date(now.getFullYear(), now.getMonth() + 1, 0, 23, 59, 59, 999);
  
     const monthlySalesAgg = await prisma.order.aggregate({
       where: {
